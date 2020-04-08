2b7edba03c2edb3543469f7d141435c65b544b66
adkeaesph <35371920+adkeaesph@users.noreply.github.com>
2020-04-08 12:58:02 +0530

    changes

--- src/com/capgemini/eLibrary/actions/LoginAction.java
+++ src/com/capgemini/eLibrary/actions/LoginAction.java
@@ -3,28 +3,14 @@ package com.capgemini.eLibrary.actions;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.DynaActionForm;
 
-/**
- *
- * @author info@tutorials4u.net
- */
-public class LoginAction extends org.apache.struts.action.Action {
+public class LoginAction extends Action {
 
-   
-
-    /**
-     * This is the action called from the Struts framework.
-     * @param mapping The ActionMapping used to select this instance.
-     * @param form The optional ActionForm bean for this request.
-     * @param request The HTTP Request we are processing.
-     * @param response The HTTP Response we are processing.
-     * @throws java.lang.Exception
-     * @return
-     */
     @Override
     public ActionForward execute(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
